2bbce68b3936e6af7cad9ed098053507c268d4c2
dnllowe <dnllowe@yahoo.com>
2018-01-17 13:55:54 -0500

    Adds beginning of good/bad test examples

---
+++ Testing-Presentation/Testing-Presentation/Calculator.cs
@@ -0,0 +1,67 @@
+﻿namespace Testing_Presentation
+{
+    public class Calculator
+    {
+        private float _currentValue = 0;
+
+        public float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public float Add(float num)
+        {
+            _currentValue += num;
+            return _currentValue;
+        }
+
+        public float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+
+        public float Divide(float num)
+        {
+            _currentValue /= num;
+            return _currentValue;
+        }
+
+        public float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public float Multiply(float num)
+        {
+            _currentValue *= num;
+            return _currentValue;
+        }
+
+        public float Square(float num)
+        {
+            return Multiply(num, num);
+        }
+
+        public void Reset()
+        {
+            _currentValue = 0;
+        }
+
+        public float Square()
+        {
+            _currentValue *= _currentValue;
+            return _currentValue;
+        }
+
+        public float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public float Subtract(float num)
+        {
+            _currentValue -= num;
+            return _currentValue;
+        }
+    }
+}
---
+++ Testing-Presentation/Testing-Presentation/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Testing_Presentation
+{
+    [TestFixture]    
+    public class CalculatorTests
+    {
+        Calculator _calculator;
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TestCase]
+        public void TestCalculator()
+        {
+            Assert.AreEqual(4, _calculator.Add(2, 2));
+            _calculator.Reset();
+            Assert.AreEqual(0, _calculator.Subtract(2, 2));
+            _calculator.Reset();
+            Assert.AreEqual(4, _calculator.Multiply(2, 2));
+            _calculator.Reset();
+            Assert.AreEqual(1, _calculator.Divide(2, 2));
+        }
+    }
+}
